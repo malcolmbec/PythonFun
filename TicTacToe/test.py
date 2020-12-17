41a675320a2bd2de8b8a177ee8bcca847ff4c0bd
malcolmbec <35633689+malcolmbec@users.noreply.github.com>
2020-12-17 14:41:51 -0500

    Add unit tests for TicTacToe

--- TicTacToe/test.py
+++ TicTacToe/test.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env/python3
 import unittest
+from unittest.mock import patch
+from io import StringIO
 
 from TicTacToe.tictactoe import TicTacToe, Player
 
@@ -13,17 +15,113 @@ class TestInit(unittest.TestCase):
 		self.assertFalse(tt.game_over)
 		self.assertIsNone(tt.winner)
 
+	def test_board_neg(self):
+		tt = TicTacToe()
+		with self.assertRaises(ValueError, msg='Board size -1x-1 is invalid.'):
+			tt._init_board(-1)
+
+	def test_board_2x2(self):
+		tt = TicTacToe()
+		with self.assertRaises(ValueError, msg='Board size 2x2 is invalid.'):
+			tt._init_board(2)
+
 	def test_board_3x3(self):
-		board = TicTacToe()._init_board()
+		tt = TicTacToe()
+		tt._init_board(3)
+		self.assertEqual(tt.size, 3)
+		self.assertEqual(tt.board, [[0, 0, 0], [0, 0, 0], [0, 0, 0]])
+
+	def test_board_5x5(self):
+		tt = TicTacToe()
+		tt._init_board(5)
+		self.assertEqual(tt.size, 5)
+		self.assertEqual(tt.board, [[0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0]])
 
 
 class TestDisplay(unittest.TestCase):
-	def test(self):
-		pass
+	def test_space(self):
+		tt = TicTacToe()
+		self.assertEqual(tt._space_to_ascii(0), ' ')
+
+	def test_space_x(self):
+		tt = TicTacToe()
+		self.assertEqual(tt._space_to_ascii(1), 'X')
+
+	def test_space_o(self):
+		tt = TicTacToe()
+		self.assertEqual(tt._space_to_ascii(2), 'O')
+
+	def test_display_new_game(self):
+		tt = TicTacToe()
+		tt._init_board(3)
+		expected = "  |   |  \n" \
+				   "---------\n" \
+				   "  |   |  \n" \
+				   "---------\n" \
+				   "  |   |  \n"
+		with patch('sys.stdout', new=StringIO()) as mock_out:
+			tt._display_board()
+			self.assertEqual(mock_out.getvalue(), expected)
+
+	def test_display_mid_game(self):
+		tt = TicTacToe()
+		tt.size = 3
+		tt.board = [[0, 1, 1], [2, 0, 2], [0, 0, 0]]
+		expected = "  | X | X\n" \
+				   "---------\n" \
+				   "O |   | O\n" \
+				   "---------\n" \
+				   "  |   |  \n"
+		with patch('sys.stdout', new=StringIO()) as mock_out:
+			tt._display_board()
+			self.assertEqual(mock_out.getvalue(), expected)
+
+	def test_display_end_game(self):
+		tt = TicTacToe()
+		tt.size = 3
+		tt.board = [[0, 1, 1], [2, 1, 2], [2, 2, 1]]
+		expected = "  | X | X\n" \
+				   "---------\n" \
+				   "O | X | O\n" \
+				   "---------\n" \
+				   "O | O | X\n"
+		with patch('sys.stdout', new=StringIO()) as mock_out:
+			tt._display_board()
+			self.assertEqual(mock_out.getvalue(), expected)
 
 
 class TestTakeTurn(unittest.TestCase):
-	def test(self):
+	def test_out_of_bounds(self):
+		tt = TicTacToe()
+		tt._init_board(3)
+		self.assertFalse(tt._out_of_bounds((0, 2)))
+
+	def test_out_of_bounds_lower(self):
+		tt = TicTacToe()
+		tt._init_board(3)
+		self.assertTrue(tt._out_of_bounds((-1, 0)))
+
+	def test_out_of_bounds_upper(self):
+		tt = TicTacToe()
+		tt._init_board(3)
+		self.assertTrue(tt._out_of_bounds((0, 3)))
+
+	def test_is_occupied(self):
+		pass
+
+	def test_is_occupied_x(self):
+		pass
+
+	def test_is_occupied_o(self):
+		pass
+
+	def test_take_turn(self):
+		pass
+
+	def take_turn_out_of_bounds(self):
+		pass
+
+	def take_turn_is_occupied(self):
 		pass
 
 
--- TicTacToe/tictactoe.py
+++ TicTacToe/tictactoe.py
@@ -6,39 +6,45 @@ class Player(Enum):
 	X = 1
 	O = 2
 
+	def __str__(self):
+		return self.name
+
 
 class TicTacToe:
 
 	def __init__(self):
-		self.size: tuple = None
+		self.size: int = None
 		self.board: list = None
 		self.turn: Player = Player.X
 		self.game_over: bool = False
 		self.winner: Player = None
 
-	def _init_board(self, size: tuple) -> list:
+	def _init_board(self, size: int) -> list:
+		if size < 3:
+			raise ValueError(f'Board size {size}x{size} is invalid.')
 		self.size = size
-		self.board = [[0] * size[1] for _ in range(size[0])]
+		self.board = [[0] * size for _ in range(size)]
 
 	@staticmethod
 	def _space_to_ascii(space: int) -> str:
 		if space:
-			return Player(space)
+			return str(Player(space))
 		else:
 			return " "
 
 	def _display_board(self):
 		for i, _ in enumerate(self.board):
 			out = ''
-			for j, space in enumerate(self.board[_]):
+			for j, space in enumerate(self.board[i]):
 				out += self._space_to_ascii(space)
-				if j < self.size[1]-1:
+				if j < self.size-1:
 					out += ' | '
-			if i < self.size[0]-1:
-				print(''.ljust(self.size[1], '-'))
+			print(out)
+			if i < self.size-1:
+				print(''.ljust(self.size**2, '-'))
 
 	def _out_of_bounds(self, pos: tuple) -> bool:
-		return pos[0] < 0 or self.size[0] <= pos[0] or pos[1] < 0 or self.size[1] <= pos[1]
+		return pos[0] < 0 or self.size <= pos[0] or pos[1] < 0 or self.size <= pos[1]
 
 	def _is_occupied(self, pos: tuple) -> bool:
 		if self._out_of_bounds(pos):
